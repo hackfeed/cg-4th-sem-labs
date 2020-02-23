0f4ea105f74da9215e7cd12a6e73039632c265b3
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-23 13:44:06 +0300

    Cleanup PAGE trash

--- lab_01/main.py
+++ lab_01/main.py
@@ -1,143 +1,85 @@
 import tkinter as tk
-import itertools as it
-import math
-import os
 
 
-def clean_entry(*entries):
-    for entry in entries:
-        entry.config(state="normal")
-        entry.delete(0, tk.END)
+class RootWindow:
+    def __init__(self, top=None):
+        img = tk.PhotoImage(file="assets/img/icon.png")
+
+        top.geometry("800x600+298+48")
+        top.minsize(1, 1)
+        top.maxsize(1351, 738)
+        top.resizable(0, 0)
+        top.title("Triangle Magic")
+        top.iconphoto(True, img)
+        top.configure(background="#000080")
+        top.configure(cursor="watch")
+        top.configure(highlightcolor="black")
+
+        self.image = tk.Canvas(top)
+        self.image.place(relx=0.263, rely=0.017, relheight=0.967, relwidth=0.725)
+
+        self.image.configure(background="#ffffff")
+        self.image.configure(borderwidth="2")
+        self.image.configure(highlightbackground="#d8d8d8")
+        self.image.configure(relief="ridge")
+        self.image.configure(selectbackground="#c4c4c4")
+
+        self.dotslist = tk.Listbox(top)
+        self.dotslist.place(relx=0.013, rely=0.088, relheight=0.53, relwidth=0.24)
+        self.dotslist.configure(background="white")
+        self.dotslist.configure(font="-family {Consolas} -size 12")
+        self.dotslist.configure(selectbackground="#c4c4c4")
+
+        self.oplabel = tk.Label(top)
+        self.oplabel.place(relx=0.013, rely=0.033, height=19, width=192)
+        self.oplabel.configure(activebackground="#f9f9f9")
+        self.oplabel.configure(background="#000080")
+        self.oplabel.configure(font="-family {Consolas} -size 14")
+        self.oplabel.configure(foreground="#ffffff")
+        self.oplabel.configure(text="Список точек")
+
+        self.addbtn = tk.Button(top)
+        self.addbtn.place(relx=0.013, rely=0.633, height=29, width=192)
+        self.addbtn.configure(background="#d9d9d9")
+        self.addbtn.configure(foreground="black")
+        self.addbtn.configure(activebackground="#000080")
+        self.addbtn.configure(font="-family {Consolas} -size 12")
+        self.addbtn.configure(text="Добавить точку")
+
+        self.delbtn = tk.Button(top)
+        self.delbtn.place(relx=0.013, rely=0.7, height=29, width=192)
+        self.delbtn.configure(background="#d9d9d9")
+        self.delbtn.configure(foreground="black")
+        self.delbtn.configure(activebackground="#000080")
+        self.delbtn.configure(font="-family {Consolas} -size 12")
+        self.delbtn.configure(text="Удалить точку")
+
+        self.editbtn = tk.Button(top)
+        self.editbtn.place(relx=0.013, rely=0.767, height=29, width=192)
+        self.editbtn.configure(background="#d9d9d9")
+        self.editbtn.configure(foreground="black")
+        self.editbtn.configure(activebackground="#000080")
+        self.editbtn.configure(font="-family {Consolas} -size 12")
+        self.editbtn.configure(text="Изменить точку")
+
+        self.clrbtn = tk.Button(top)
+        self.clrbtn.place(relx=0.013, rely=0.833, height=29, width=192)
+        self.clrbtn.configure(background="#d9d9d9")
+        self.clrbtn.configure(foreground="black")
+        self.clrbtn.configure(activebackground="#000080")
+        self.clrbtn.configure(font="-family {Consolas} -size 12")
+        self.clrbtn.configure(text="Очистить всё")
+
+        self.solvebtn = tk.Button(top)
+        self.solvebtn.place(relx=0.013, rely=0.9, height=29, width=192)
+        self.solvebtn.configure(background="#d9d9d9")
+        self.solvebtn.configure(foreground="black")
+        self.solvebtn.configure(activebackground="#000080")
+        self.solvebtn.configure(font="-family {Consolas} -size 12")
+        self.solvebtn.configure(text="Решить задачу")
 
 
-def about(root):
-    """ Вывод окна "О программе" """
-    about_window = tk.Toplevel(root)
-    about_window.grab_set()
-    about_window.iconbitmap("icon.png")
-    about_window.geometry("330x200+425+250")
-    about_window.resizable(False, False)
-    about_window.title("О TriangleDots")
-    about_window.config(bg="#000080")
-
-    about_label1 = tk.Label(about_window,
-                            text="\nВизуализация треугольника\n "
-                            "содержащего одинаковое количество точек\n"
-                            "из двух различных множеств,\n"
-                            "построенного на точках из первого множества"
-                            "\n\n"
-                            "Kononenko Sergey ICS7-23B",
-                            font="consolas 10",
-                            bg="#000080",
-                            fg="white")
-    about_label1.pack()
-
-    about_label2 = tk.Label(about_window,
-                            text="@hackfeed",
-                            font="consolas 10 bold",
-                            bg="white",
-                            fg="#000080")
-    about_label2.pack()
-
-    exit_about = tk.Button(about_window, text="Выйти",
-                           width=6,
-                           height=2,
-                           font="consolas 10 bold",
-                           bg="#000080",
-                           fg="white",
-                           relief="flat",
-                           command=lambda: wexit(about_window))
-    exit_about.pack()
-
-
-def wexit(root):
-    """ Выход из программы. """
-    root.destroy()
-
-
-def main():
-    """ Создание каскада окна программы. """
-    root = tk.Tk()
-    root.iconbitmap("icon.png")
-    root.geometry("400x200+400+200")
-    root.resizable(False, False)
-    root.title("TriangleDots")
-
-    """ Создание меню. """
-    main_menu = tk.Menu(root)
-    root.config(menu=main_menu, bg="#000080")
-
-    clean_menu = tk.Menu(main_menu, tearoff=0)
-    clean_menu.add_command(label="Очистить поле ввода точек первого множества",
-                           command=lambda: clean_entry(first_dot_set_entry))
-    clean_menu.add_command(label="Очистить поле ввода точек второго множества",
-                           command=lambda: clean_entry(second_dot_set_entry))
-    clean_menu.add_separator()
-    clean_menu.add_command(label="Очистить поле ввода точек обоих множеств",
-                           command=lambda: clean_entry(first_dot_set_entry,
-                                                       second_dot_set_entry))
-
-    action_menu = tk.Menu(main_menu, tearoff=0)
-    action_menu.add_command(label="Визуализировать",
-                            command=lambda: draw_triangle(first_dot_set_entry,
-                                                          second_dot_set_entry))
-
-    about_menu = tk.Menu(main_menu, tearoff=0)
-    about_menu.add_command(label="О программе", command=lambda: about())
-
-    main_menu.add_cascade(label="Визуализация", menu=action_menu)
-    main_menu.add_cascade(label="Очистка", menu=clean_menu)
-    main_menu.add_cascade(label="Справка", menu=about_menu)
-
-    """ Организация UI. """
-    welcome_label = tk.Label(root,
-                             text="Нахождение магического треугольника",
-                             font="consolas 10 bold",
-                             bg="white",
-                             fg="#000080")
-    welcome_label.place(x=200, y=20, anchor="center")
-
-    first_dot_set_label = tk.Label(root,
-                                   text="Введите координаты точек первого множества",
-                                   font="consolas 10 bold",
-                                   bg="#000080",
-                                   fg="white")
-    first_dot_set_label.place(x=200, y=45, anchor="center")
-
-    first_dot_set_start = "x1 y1 ..."
-    first_dot_set_entry = tk.Entry(root, width=60)
-    first_dot_set_entry.insert(0, first_dot_set_start)
-    first_dot_set_entry.place(x=200, y=65, anchor="center")
-
-    second_dot_set_label = tk.Label(root,
-                                    text="Введите координаты точек второго множества",
-                                    font="consolas 10 bold",
-                                    bg="#000080",
-                                    fg="white")
-    second_dot_set_label.place(x=200, y=85, anchor="center")
-
-    second_dot_set_start = "x1 y1 ..."
-    second_dot_set_entry = tk.Entry(root, width=60)
-    second_dot_set_entry.insert(0, second_dot_set_start)
-    second_dot_set_entry.place(x=200, y=105, anchor="center")
-
-    draw_button = tk.Button(root, text="Визуализировать",
-                            width=16,
-                            height=2,
-                            font="consolas 10 bold",
-                            bg="white",
-                            fg="#0ad325",
-                            command=lambda: draw_triangle(first_dot_set_entry,
-                                                          second_dot_set_entry))
-    draw_button.place(x=130, y=145, anchor="center")
-
-    exit_button = tk.Button(root, text="Выйти",
-                            width=16,
-                            height=2,
-                            font="consolas 10 bold",
-                            bg="white",
-                            fg="#ff0000",
-                            command=lambda: wexit(root))
-    exit_button.place(x=270, y=145, anchor="center")
-
-    root.mainloop()
+if __name__ == "__main__":
+    ROOT = tk.Tk()
+    TOPLEVEL = RootWindow(ROOT)
+    ROOT.mainloop()
